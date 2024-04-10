2a73f0e9be51ae48007949dcf993aa02489e3ee1
Gaetano Mondelli <gaetano.mondelli@gmail.com>
2024-04-10 22:41:37 +0100

    feat: added script to deploy new bridges when required

--- README.md
+++ README.md
@@ -2,6 +2,9 @@
 https://www.encode.club/scaling-web3-hackathon
 
 
+gh repo clone hyperlane-xyz/hyperlane-monorepo
+
+
 hyperlane deploy core \
     --targets etherlink,sepolia
     --chains ./configs/chains.yaml
@@ -14,6 +17,8 @@ rust % CONFIG_FILES=./agent-config-2024-04-04-16-16-27.json cargo run --release 
     --checkpointSyncer.type localStorage \
     --checkpointSyncer.path ./signer-etherlink \
     --validator.key "0x1294695293f333466d699cca83fce35cf2c3dd960fd35a93d44ae548835c9b32"
+
+
     rust % CONFIG_FILES=./agent-config-2024-04-04-16-16-27.json cargo run --release --bin validator -- \
     --db ../hyperlane_db_validator_etherlink \
     --originChainName etherlink \
---
+++ create_bridge.sh
@@ -0,0 +1,92 @@
+
+#!/bin/bash
+if [ ! -d "hyperlane-monorepo" ]; then
+    echo "Directory hyperlane-monorepo does not exist."
+    gh repo clone hyperlane-xyz/hyperlane-monorepo
+fi
+
+export HYP_KEY="0x1294695293f333466d699cca83fce35cf2c3dd960fd35a93d44ae548835c9b32"
+
+echo $HYP_KEY
+
+echo "Directory hyperlane-monorepo exists." 
+cd hyperlane-monorepo/rust
+
+export CONFIG_FOLDER=../../bridge/configs
+export ART_FOLDER=../../bridge/artifacts
+export VALIDATOR_SIGNATURES_DIR=../../bridge/validator_signatures
+export DB_RELAYER=../../bridge/hyperlane_db_relayer
+export DB_VALIDATOR=../../bridge/hyperlane_db_validator
+
+
+
+if [ ! -d "$CONFIG_FOLDER" ]; then
+    echo "Directory $CONFIG_FOLDER does not exist."
+    exit 1
+fi
+
+rustup --version
+
+# if the artifacts folder contains files skip the deploy
+
+if [ "$(ls -A $ART_FOLDER)" ]; then
+echo "Artifacts folder is not empty. Skipping deploy."
+else
+echo "Artifacts folder is empty. Deploying."
+
+rm -rf $DB_RELAYER
+rm -rf $DB_VALIDATOR
+
+hyperlane deploy core \
+    --targets etherlink,sepolia \
+    --chains $CONFIG_FOLDER/chains.yaml \
+    --ism $CONFIG_FOLDER/ism.yaml \
+    --out $ART_FOLDER \
+    --key $HYP_KEY  \
+    --yes 
+
+fi
+
+
+
+export ARG_FILE=$(ls -t $ART_FOLDER | grep -E "agent-config-[0-9]{4}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}-[0-9]{2}.json" | head -n 1)
+
+export CONFIG_FILES=$ART_FOLDER/$ARG_FILE
+
+echo $CONFIG_FILES
+
+ls -l $CONFIG_FILES
+
+
+cleanup() {
+    echo "Cleaning up and terminating processes..."
+    kill "$pid1" "$pid2"
+    exit
+}
+
+trap cleanup SIGINT
+
+
+
+# Start the first process (validator) in the background
+cargo run --release --bin validator -- \
+    --db $DB_VALIDATOR \
+    --originChainName etherlink \
+    --checkpointSyncer.type localStorage \
+    --checkpointSyncer.path $VALIDATOR_SIGNATURES_DIR \
+    --validator.key $HYP_KEY &
+pid1=$!
+
+# # Start the second process (relayer) in the background
+cargo run --release --bin relayer -- \
+    --db $DB_RELAYER \
+    --relayChains etherlink,sepolia \
+    --allowLocalCheckpointSyncers true \
+    --defaultSigner.key $HYP_KEY \
+    --metrics-port 9091 &
+pid2=$!
+
+# Wait for both processes to finish
+wait "$pid1" "$pid2"
+
+echo "Both processes have finished."
